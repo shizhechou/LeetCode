096960d7eac056d6a7d5cd45ababc5aad9c9dc83
George.Chou <shizhe.zhou@gmail.com>
2016-04-05 15:03:55 +0800

    LeetCode 258.AddDigits

---
+++ Leetcode/src/algorithm/AddDigits/java/AddDigits.java
@@ -0,0 +1,24 @@
+package algorithm.AddDigits.java;
+
+import java.util.Scanner;
+
+public class AddDigits {
+
+	public void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			System.out.print("Enter the positive number:");
+			int num = scanner.nextInt();
+			int result = addDigits(num);
+			System.out.println("Result is " + result + ".");
+		} finally {
+			scanner.close();
+		}
+
+	}
+
+	private int addDigits(int num) {
+		return num > 9 ? (num % 9 == 0 ? 9 : num % 9) : num;
+	}
+
+}
