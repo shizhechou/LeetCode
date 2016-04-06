ee214ff985d99d94d24d716d81b3707d0855c004
George.Chou <shizhe.zhou@gmail.com>
2016-04-06 21:49:30 +0800

    Add TreeNode

--- Leetcode/src/algorithm/MaxDepthBinaryTree/java/MaxDepthBinaryTree.java
+++ Leetcode/src/algorithm/MaxDepthBinaryTree/java/MaxDepthBinaryTree.java
@@ -3,9 +3,15 @@ package algorithm.MaxDepthBinaryTree.java;
 import algorithm.TreeNode;
 
 public class MaxDepthBinaryTree {
-	public int maxDepth(TreeNode root) {
+	public static int maxDepth(TreeNode root) {
 		if (root == null)
 			return 0;
 		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 	}
+	
+	public static void main(String[] args) {
+        TreeNode tree = TreeNode.build(new Integer[] {1,2,3,4,null,null,5});
+        tree.print();
+        System.out.println("The max depth is : "+maxDepth(tree));
+    }
 }
--- Leetcode/src/algorithm/TreeNode.java
+++ Leetcode/src/algorithm/TreeNode.java
@@ -1,10 +1,51 @@
 package algorithm;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class TreeNode {
-	int val;
+	public int val;
 	public TreeNode left;
 	public TreeNode right;
-	TreeNode(int x){
+
+	public TreeNode(int x) {
 		val = x;
 	}
+
+	public static TreeNode build(Integer[] data) {
+		return build(1, data);
+	}
+
+	private static TreeNode build(int index, Integer[] data) {
+		if (index > data.length) {
+			return null;
+		}
+
+		if (data[index - 1] == null) {
+			return null;
+		}
+
+		TreeNode result = new TreeNode(data[index - 1]);
+		result.left = build(index * 2, data);
+		result.right = build(index * 2 + 1, data);
+
+		return result;
+	}
+
+	public void print() {
+		Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+		queue.add(this);
+		while (!queue.isEmpty()) {
+			TreeNode node = queue.poll();
+			if (node.left != null) {
+				queue.add(node.left);
+			}
+			if (node.right != null) {
+				queue.add(node.right);
+			}
+			System.out.print(node.val + " ");
+		}
+		System.out.println();
+	}
+
 }
