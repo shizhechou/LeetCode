91764c3a9941e970596060b6a86e6300fc8f1d1c
George.Chou <shizhe.zhou@gmail.com>
2016-04-11 20:22:41 +0800

    Add ListNode class

--- Leetcode/src/algorithm/DeleteNode/java/DeleteNode.java
+++ Leetcode/src/algorithm/DeleteNode/java/DeleteNode.java
@@ -1,12 +1,26 @@
 package algorithm.DeleteNode.java;
 
+import algorithm.ListNode;
+
 public class DeleteNode {
-	public void deleteNode(ListNode node) {
-        ListNode n1 = null;
-        n1 = node.next;
-        if(n1 != null){
-            node.val = n1.val;
-            node.next = n1.next;
-        }
-    }
+	public static void deleteNode(ListNode node) {
+		ListNode n1 = null;
+		n1 = node.next;
+		if (n1 != null) {
+			node.val = n1.val;
+			node.next = n1.next;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ListNode listNode = ListNode.build(new Integer[] { 4, 2, 7, 1, 3, 6, 9 });
+		// Original ListNode list
+		listNode.print();
+		// Get delete ListNode
+		ListNode deleteNode = ListNode.getListNode(listNode, 2);
+		deleteNode.print();
+		// After deleteNode
+		deleteNode(deleteNode);
+		listNode.print();
+	}
 }
---
+++ Leetcode/src/algorithm/ListNode.java
@@ -0,0 +1,66 @@
+package algorithm;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class ListNode {
+	public int val;
+	public ListNode next;
+	public static int length;
+
+	public ListNode() {
+	}
+
+	ListNode(int x) {
+		val = x;
+	}
+	
+	public static ListNode build(Integer[] data) {
+		ListNode result = null;
+		ListNode current = null;
+		
+		for(int i : data){
+			ListNode node = new ListNode(i);
+			if(result == null){
+				result = node;
+				current = result;
+			}else{
+				ListNode tmp = new ListNode(i);
+				current.next = tmp;
+				current = tmp;
+			}
+			length++;
+		}
+		return result;
+	}
+	
+	public static ListNode getListNode(ListNode first, int pos){
+		if(pos >= first.length){
+			return null;
+		}
+		
+		ListNode result = first;
+		for(int i=1;i<pos;i++){
+			result = result.next;
+		}
+		return result;
+	}
+	
+	private int getListNodeLength(ListNode first){
+		return first.length;
+	}
+	
+	public void print() {
+		Queue<ListNode> queue = new ArrayDeque<ListNode>();
+		queue.add(this);
+		while (!queue.isEmpty()) {
+			ListNode node = queue.poll();
+			if (node.next != null) {
+				queue.add(node.next);
+			}
+
+			System.out.print(node.val + " ");
+		}
+		System.out.println();
+	}
+}
