d547c26a63022048cf5988b38c01ad84317bb17a
George.Chou <shizhe.zhou@gmail.com>
2016-04-16 09:37:54 +0800

    169. Majority Element

---
+++ Leetcode/src/algorithm/MajorityElement/java/MajorityElement.java
@@ -0,0 +1,22 @@
+package algorithm.MajorityElement.java;
+
+import java.util.Arrays;
+
+import algorithm.TreeNode;
+
+public class MajorityElement {
+	public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+	
+	public static void main(String[] args) {
+		// Test info
+        int test1[] = {1,3,4,1,1};
+        int test2[] = {3,7,2,1,2,3,5,2,2};
+        System.out.println("test1:"+test1.toString());
+        System.out.println("test1 Majority Element:"+majorityElement(test1));
+        System.out.println("test2:"+test2.toString());
+        System.out.println("test2 Majority Element:"+majorityElement(test2));
+    }
+}
