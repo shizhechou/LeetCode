ff259640e4ea94523475269352c4bf3f5171b720
George.Chou <shizhe.zhou@gmail.com>
2016-05-10 08:54:17 +0800

    24. Swap Nodes in Pairs

---
+++ Leetcode/src/algorithm/SwapNodesInPairs/java/SwapNodesInPairs.java
@@ -0,0 +1,27 @@
+package algorithm.SwapNodesInPairs.java;
+
+import algorithm.ListNode;
+
+public class SwapNodesInPairs {
+
+	public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+        	return head;
+        }
+        
+        ListNode cur = head;
+        ListNode pre = null;
+        head = cur.next;
+        while(cur != null && cur.next != null){
+        	ListNode tmp = cur.next;
+        	cur.next = tmp.next;
+        	tmp.next = cur;
+        	if(pre != null){
+        		pre.next = tmp;
+        	}
+        	pre = cur;
+        	cur = cur.next;
+        }
+        return head;
+    }
+}
