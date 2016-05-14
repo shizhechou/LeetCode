dca1a1a27553fcbfefa091fb180bcc983e83a61f
George.Chou <shizhe.zhou@gmail.com>
2016-05-14 09:06:04 +0800

    19. Remove Nth Node From End of List

---
+++ Leetcode/src/algorithm/RemoveNthNodeFromEndOfList/java/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,25 @@
+package algorithm.RemoveNthNodeFromEndOfList.java;
+
+
+
+public class algorithm.RemoveNthNodeFromEndOfList {
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+        	ListNode fast = head;
+        	ListNode slow = head;
+        
+        	for(int i =0 ; i< n ;i++){
+            		fast = fast.next;
+        	}
+        	if(fast == null){
+            		head = head.next;
+            		return head;
+        	}
+        	while(fast.next != null){
+            		fast = fast.next;
+            		slow = slow.next;
+        	}
+        
+        	slow.next = slow.next.next;
+        	return head;
+    }
+}
