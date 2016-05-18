2bb6d56cb579775a46aaa7287873a5b646f757a5
George.Chou <shizhe.zhou@gmail.com>
2016-05-18 09:09:43 +0800

    190. Reverse Bits

---
+++ Leetcode/src/algorithm/ReverseBits/java/ReverseBits.java
@@ -0,0 +1,19 @@
+package algorithm.ReverseBits.java;
+
+public class ReverseBits {
+
+	public int reverseBits(int n) {
+		if( n == 0)
+			return 0;
+		
+		int result = 0;
+		for (int i = 0; i < 32; i++) {
+			result <<= 1;
+			if ((n & 1) == 1) {
+				result += 1;
+			}
+			n >>= 1;
+		}
+		return result;
+	}
+}
