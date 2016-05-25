efc72b7ad598d62e65a1bb9320c36c988080f8bc
George.Chou <shizhe.zhou@gmail.com>
2016-05-25 13:38:44 +0800

    28. Implement strStr()

---
+++ Leetcode/src/algorithm/ImplementstrStr/java/ImplementstrStr.java
@@ -0,0 +1,9 @@
+package algorithm.ImplementstrStr.java;
+
+public class ImplementstrStr {
+
+	public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
