963a60e2cbbf906891a9afcd67662f88101c413e
George.Chou <shizhe.zhou@gmail.com>
2016-06-19 23:30:33 +0800

    160. Intersection of Two Linked Lists

---
+++ Leetcode/src/algorithm/IntersectionOfTwoLinkedLists/java/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,19 @@
+package algorithm.IntersectionOfTwoLinkedLists.java;
+
+import algorithm.ListNode;
+
+public class IntersectionOfTwoLinkedLists {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null) {
+			return null;
+		}
+		ListNode a = headA;
+		ListNode b = headB;
+		while (a != b) {
+			a = (a == null) ? headB : a.next;
+			b = (b == null) ? headA : b.next;
+		}
+
+		return a;
+	}
+}
