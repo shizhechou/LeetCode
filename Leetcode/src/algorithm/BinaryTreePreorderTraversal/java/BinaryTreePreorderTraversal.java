d0f0a81f28aaef9ddf1cb81297a82acf789e2a4c
George.Chou <shizhe.zhou@gmail.com>
2016-06-26 12:23:19 +0800

    144. Binary Tree Preorder Traversal
    Add Iterative method

--- Leetcode/src/algorithm/BinaryTreePreorderTraversal/java/BinaryTreePreorderTraversal.java
+++ Leetcode/src/algorithm/BinaryTreePreorderTraversal/java/BinaryTreePreorderTraversal.java
@@ -2,17 +2,38 @@ package algorithm.BinaryTreePreorderTraversal.java;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import algorithm.TreeNode;
 
 public class BinaryTreePreorderTraversal {
+	// Recursive method
 	public List<Integer> preorderTraversal(TreeNode root) {
 		List<Integer> result = new ArrayList<Integer>();
-        if (root != null){
-            result.add(root.val);
-            result.addAll(preorderTraversal(root.left));
-            result.addAll(preorderTraversal(root.right));
-        }
+        addNode(result, root);
         return result;
     }
+	
+	public void addNode(List<Integer> list, TreeNode root) {
+		if (root != null){
+			list.add(root.val);
+			addNode(list, root.left);
+			addNode(list, root.right);
+        }
+	}
+	// Iterative method
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> result = new ArrayList<Integer>();
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		if(root == null) return result;
+		stack.push(root);
+		
+		while(!stack.isEmpty()){
+			TreeNode node = stack.pop();
+			result.add(node.val);
+			if (node.right != null) stack.push(node.right);
+			if (node.left != null) stack.push(node.left);
+		}
+		return result;
+	}
 }
