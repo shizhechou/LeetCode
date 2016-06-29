d90fdf22f47c7e07e3031a6c0ddab2fa61b4a779
George.Chou <shizhe.zhou@gmail.com>
2016-06-29 22:36:01 +0800

    230. Kth Smallest Element in a BST

---
+++ Leetcode/src/algorithm/KthSmallestElementBST/java/KthSmallestElementBST.java
@@ -0,0 +1,23 @@
+package algorithm.KthSmallestElementBST.java;
+
+import algorithm.TreeNode;
+
+public class KthSmallestElementBST {
+	public static int ans = 0;
+	public int kthSmallest(TreeNode root, int k) {
+	    helper(root, k);
+	    return ans;
+	}
+
+	public int helper(TreeNode root, int k) {
+	    if (root == null) {
+	        return 0;
+	    }
+	    int leftCount = helper(root.left, k);
+	    int rightCount = helper(root.right, k - leftCount - 1);
+	    if (k == leftCount + 1) {
+	        ans = root.val;
+	    }
+	    return leftCount + rightCount + 1;
+	}
+}
