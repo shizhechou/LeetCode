644794b90b853a27c7dc0798c40c20acc85c32b9
George.Chou <shizhe.zhou@gmail.com>
2016-07-02 17:23:32 +0800

    337. House Robber III

---
+++ Leetcode/src/algorithm/HouseRobberIII/java/HouseRobberIII.java
@@ -0,0 +1,23 @@
+package algorithm.HouseRobberIII.java;
+
+import algorithm.TreeNode;
+
+public class HouseRobberIII {
+	public int rob(TreeNode root) {
+        int[] maxVal = dpRob(root);
+        return Math.max(maxVal[0], maxVal[1]);
+    }
+
+    public int[] dpRob(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0};
+        } else {
+            int[] maxVal = new int[2];//maxVal[0] store the max value without robing current node, maxVal[1] store the max value with robing current node,
+            int[] leftMax = dpRob(root.left);
+            int[] rightMax = dpRob(root.right);
+            maxVal[0] = Math.max(leftMax[0], leftMax[1]) + Math.max(rightMax[0], rightMax[1]);
+            maxVal[1] = leftMax[0] + rightMax[0] + root.val;
+            return maxVal;
+        }
+    }
+}
