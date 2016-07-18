bbdd2e3c24e2574c7c2ac399b52804eb746d103a
George.Chou <shizhe.zhou@gmail.com>
2016-07-18 22:51:02 +0800

    367. Valid Perfect Square

---
+++ Leetcode/src/algorithm/ValidPerfectSquare/java/ValidPerfectSquare.java
@@ -0,0 +1,12 @@
+package algorithm.ValidPerfectSquare.java;
+
+public class ValidPerfectSquare {
+	public boolean isPerfectSquare(int num) {
+		int i = 1;
+		while (num > 0) {
+			num -= i;
+			i += 2;
+		}
+		return num == 0;
+	}
+}
