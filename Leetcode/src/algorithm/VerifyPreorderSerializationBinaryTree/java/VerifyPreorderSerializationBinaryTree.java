0d3faa0d8d708273333a7e7dfbd9dd857a089819
George.Chou <shizhe.zhou@gmail.com>
2016-08-19 00:13:59 +0800

    331. Verify Preorder Serialization of a Binary Tree

---
+++ Leetcode/src/algorithm/VerifyPreorderSerializationBinaryTree/java/VerifyPreorderSerializationBinaryTree.java
@@ -0,0 +1,15 @@
+package algorithm.VerifyPreorderSerializationBinaryTree.java;
+
+public class VerifyPreorderSerializationBinaryTree {
+	public boolean isValidSerialization(String preorder) {
+		String nodes[] = preorder.split(",");
+		int diff = 1;
+		for (String node : nodes) {
+			if (--diff < 0)
+				return false;
+			if (!node.equals("#"))
+				diff += 2;
+		}
+		return diff == 0;
+    }
+}
