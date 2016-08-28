bc8eff4d7d56bfd032ba834f63b84621eaea7c6c
George.Chou <shizhe.zhou@gmail.com>
2016-08-28 11:48:45 +0800

    114. Flatten Binary Tree to Linked List

---
+++ Leetcode/src/algorithm/FlattenBinaryTreeToLinkedList/java/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,23 @@
+package algorithm.FlattenBinaryTreeToLinkedList.java;
+
+import algorithm.TreeNode;
+
+public class FlattenBinaryTreeToLinkedList {
+	public void flatten(TreeNode root) {
+        if(root==null)
+        	return;
+        
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        
+        root.left = null;
+        flatten(left);
+        flatten(right);
+        
+        root.right = left;
+        TreeNode cur = root;
+        while(cur.right != null)
+        	cur = cur.right;
+        cur.right = right;
+    }
+}
